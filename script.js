a3ddae193d88899d6d6e76f3a1282c0300f2a93b
shef3r <brunorek10@gmail.com>
2023-12-21 16:58:10 +0100

    added more animations

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-function goback(element) {
-    element.style.animation = "forwards 0.5s smallerlink";
-}
-
-function goin(element) {
-    element.style.animation = "forwards 0.5s biggerlink";
-}
-
-function gobold(element) {
-    element.style.animation = "forwards 0s bold";
-}
-
-function gonobold(element) {
-    element.style.animation = "forwards 0s nobold";
-}
+function goback(element) { element.style.animation = "forwards 0.5s smallerlink"; }
+function goin(element) { element.style.animation = "forwards 0.5s biggerlink"; }
+function bloom(element) { element.style.animation = "forwards 0.5s bloom"; }
+function nobloom(element) { element.style.animation = "forwards 0.5s nobloom"; }
+function round(element) { element.style.animation = "forwards 0.2s round"; }
+function unround(element) { element.style.animation = "forwards 0.2s noround"; }
